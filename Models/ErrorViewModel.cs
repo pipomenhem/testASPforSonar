e19a36a0d88ed502f710d8a7eac40a2ccdf9fb6b
pipomenhem <96459261+pipomenhem@users.noreply.github.com>
2022-06-28 14:18:44 +0300

    Add files via upload

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace applicationTEST1_ForSonarqube.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
